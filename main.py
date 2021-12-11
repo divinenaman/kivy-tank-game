c69e20fc45ab67ff04808c2e902d0346f65e80ee
Naman Agarwal <namanddev7@gmail.com>
2021-12-11 21:56:53 +0530

    intial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+class FirstApp(App):
+    pass
+
+if __name__=="__main__":
+    FirstApp().run()
