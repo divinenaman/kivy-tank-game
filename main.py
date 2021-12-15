79b442effdd8c3b993e4ceb78e34059f3ff1d17f
Naman Agarwal <namanddev7@gmail.com>
2021-12-15 23:56:48 +0530

    learning

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 
+class Increment(Widget):
+
+    def inc(self):
+        self.count = self.count + 1 
+
 class FirstApp(App):
     pass
 
